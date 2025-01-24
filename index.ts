bfaf337d2aac47053dd1798b87016200f05b06a0
Sachin Raj R <sachin@Sachins-Laptop.local>
2025-01-23 23:25:20 -0500

    01-setup-success

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+const z: number = 10;
+
+function read(text: string | number): string | number {
+    return text;
+}
+
+const msg: string | number = read("Hello from Bun!");
+
+console.log(msg);
